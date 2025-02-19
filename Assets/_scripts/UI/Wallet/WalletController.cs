961b41b5826ab071f9bb3d87b91ac6d491de7e00
CamiloGato <cm.andres022@hotmail.com>
2025-02-19 10:42:34 -0500

    Update: Updated code

--- Assets/_scripts/UI/Wallet/WalletController.cs
+++ Assets/_scripts/UI/Wallet/WalletController.cs
@@ -7,22 +7,29 @@ namespace _scripts.UI.Wallet
     {
         [SerializeField] private WalletView walletView;
         [SerializeField] private int startingCoins;
-
+        
         private WalletManager _walletManager;
-
+        
         private void Awake()
         {
             _walletManager = new WalletManager(startingCoins);
             _walletManager.OnCoinsChanged += walletView.UpdateCoinView;
             _walletManager.Initialize();
         }
-
+        
         private void OnDestroy()
         {
             _walletManager.OnCoinsChanged -= walletView.UpdateCoinView;
         }
         
-         public void AddCoins(int amount) => _walletManager.AddCoins(amount);
-         public void SpendCoins(int amount) => _walletManager.SpendCoins(amount);
+        public void AddCoins(int amount)
+        {
+            _walletManager.AddCoins(amount);
+        }
+         
+        public void SpendCoins(int amount)
+        {
+            _walletManager.SpendCoins(amount);
+        }
     }
 }
--- Assets/_scripts/UI/Wallet/WalletView.cs
+++ Assets/_scripts/UI/Wallet/WalletView.cs
@@ -6,7 +6,7 @@ namespace _scripts.UI.Wallet
     public class WalletView : MonoBehaviour
     {
         [SerializeField] private TextMeshProUGUI coinText;
-
+        
         public void UpdateCoinView(int amount)
         {
             coinText.text = $"Coins: {amount}";
